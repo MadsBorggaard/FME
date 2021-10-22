d9303235c89a733002fc4467be2280f4d25c30c4
MadsBorggaard <mads.borggaard@outlook.dk>
2021-10-22 15:40:01 +0200

    Create Startup Python Script.py

---
+++ Start-Stop Scripts/Startup Python Script.py
@@ -0,0 +1,15 @@
+import os, uuid, pyodbc
+import fme
+import pathlib
+
+logFile = fme.logFileName
+path = pathlib.Path(fme.getAbsolutePath(logFile)).parent.resolve()
+
+uuidFile = os.path.join(path, 'uuid.txt')
+local_uuid = ''
+if not os.path.isfile(uuidFile):
+    with open(uuidFile, 'w') as f:
+        local_uuid = uuid.uuid4()
+        f.write(str(local_uuid))
+        
+print(local_uuid)        
