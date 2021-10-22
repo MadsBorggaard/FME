f6e06704a889694bd43542f034e72849515ccdc1
MadsBorggaard <mads.borggaard@outlook.dk>
2021-10-22 15:54:14 +0200

    Create FME_Statestik.sql

---
+++ Start-Stop Scripts/FME_Statestik.sql
@@ -0,0 +1,16 @@
+CREATE TABLE [dbo].[FME_Statestik](
+	[dato] [datetime2](7) NOT NULL,
+	[id] [nchar](36) NULL,
+	[name] [nvarchar](max) NULL,
+	[status] [nvarchar](10) NULL,
+	[elapsedRunTime] [float] NULL,
+	[cpuSysTime] [float] NULL,
+	[cpuUserTime] [float] NULL,
+	[cpuTime] [float] NULL,
+	[failureMessage] [nvarchar](max) NULL,
+	[featuresRead] [nvarchar](max) NULL,
+	[featuresWritten] [nvarchar](max) NULL,
+	[totalFeaturesRead] [bigint] NULL,
+	[totalFeaturesWritten] [bigint] NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
